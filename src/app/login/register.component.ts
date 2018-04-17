05c5a7b171038c0bf6915392f8716b185044f5a8
luisenktt <kttweb@gmail.com>
2018-04-17 13:03:38 +0200

    Primer Commit

---
+++ src/app/login/register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'map-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/nopagefound/nopagefound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'map-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styles: []
+})
+export class NopagefoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
